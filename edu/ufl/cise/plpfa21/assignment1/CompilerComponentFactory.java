adb988e7ae1062a33afc9af442348b5f920fe698
Ddoublej917 <89986686+Ddoublej917@users.noreply.github.com>
2021-09-05 19:33:17 -0400

    Realized that I need a Lexer class lmao

--- edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
+++ edu/ufl/cise/plpfa21/assignment1/CompilerComponentFactory.java
@@ -1,32 +1,128 @@
 package edu.ufl.cise.plpfa21.assignment1;
 
-public class CompilerComponentFactory {
-	
-	public class Token implements IPLPToken(
-			Kind kind,
-			int pos,
-			int length,
-			int line,
-			int posInLine
-			) {
-		//required methods here
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import edu.ufl.cise.plpfa21.assignment1.PLPTokenKinds.Kind;
+
+public class CompilerComponentFactory{
+	static public class Token implements IPLPToken {
+		Kind token_kind;
+		int token_pos;
+		int token_length;
+		int token_line;
+		int token_posInLine;
+		int num;
+		String token_text;
+		Token next;
+		
+		public Token (Kind kind, int pos, int length, int line, int posInLine, String text){
+			token_kind = kind;
+			token_pos = pos;
+			token_length = length;
+			token_line = line;
+			token_posInLine = posInLine;
+			token_text = text;
+			next = null;
+		}
+		
+		@Override
+		public Kind getKind() {
+			return token_kind;
+		}
+
+
+		@Override
+		public String getText() {
+			return token_text;
+		}
+
+
+		@Override
+		public int getLine() {
+			return token_line;
+		}
+
+
+		@Override
+		public int getCharPositionInLine() {
+			return token_posInLine;
+		}
+
+
+		@Override
+		public String getStringValue() { 
+			token_text = token_text.replaceAll("[\"]", "");  
+			return null;
+		}
+
+
+		@Override
+		public int getIntValue() {
+			num = Integer.parseInt(token_text);
+			return num;
+		}
 	}
 	
-	private final ArrayList<IPLPToken> tokens;
-	private final char[] chars; //holds characters with 0 at the end.
-	private int nextTokenPos = 0;
-	static final char EOFchar = 0;
+	static public class Lexer implements IPLPLexer {
+		Token head;
+		Token current;
+		public Lexer (Token t){
+			head = null;
+		}
+		
+		public void add (Token t) {
+			Token newToken  = new Token(t.token_kind, t.token_pos, t.token_length, t.token_line, t.token_posInLine, t.token_text);
+			if(head == null) {
+				head = newToken;
+				return;
+			}
+			else {
+				current = head;
+				while(current.next != null) {
+					current = current.next;
+				}
+					
+				t.next = newToken;
+			}
+			
+		}
+
+		@Override
+		public IPLPToken nextToken() throws LexicalException {
+			if(current == head) {
+				return head;
+			}
+			else {
+				current = current.next;
+				return current;
+			}
+			
+		}
+	}
+		
 
 	static IPLPLexer getLexer(String input) {
-		//TODO  create and return a Lexer instance to parse the given input.
-		int numChars = inputString.length();
-		this.chars = Arrays.copyOf(input.toCharArray(), numChars + 1); // input char array terminated with EOFchar for convenience
+		Lexer result;
+		char[] chars; //holds characters with 0 at end
+		char EOFchar = 0;
+		int startPos = 0;
+		int startPosInLine = 0;
+		int pos = 0;
+		int line = 0;
+		int posInLine = 0;
+		int nextTokenPos = 0;
+		int numTokens = 0;
+		String txt = ""; //temporary string to hold text of tokens
+		int numChars = input.length();
+		chars = Arrays.copyOf(input.toCharArray(), numChars + 1); 
 		chars[numChars] = EOFchar;
-		tokens = new ArrayList<>();
 		
-		private enum State {START, HAVE_EQUAL, DIGITS, IDENT_PART}
+		result = new Lexer(new Token(Kind.INT_LITERAL, startPos, 1, line, startPosInLine, txt));
+		result.head = new Token(Kind.INT_LITERAL, startPos, 1, line, startPosInLine, txt);
+		enum State {START, HAVE_EQUAL, DIGITS, IDENT_PART}
 		
-		State state = START;
+		State state = State.START;
 		
 		while(pos < chars.length) {
 			char ch = chars[pos]; //get current char
@@ -34,7 +130,7 @@ public class CompilerComponentFactory {
 				case START-> {
 					startPos = pos;
 					switch (ch) {
-						case '', '\t'-> {
+						case ' ', '\t'-> {
 							pos++;
 							posInLine++;
 						}
@@ -44,7 +140,8 @@ public class CompilerComponentFactory {
 							posInLine = 1;
 						}
 						case '0'-> {
-							tokens.add(new Token(Kind.NUMLIT, startPos, 1, line, startPosInLine));
+							result.add(new Token(Kind.INT_LITERAL, startPos, 1, line, startPosInLine, txt));
+							result.current = result.current.next;
 							pos++;
 							posInLine++;
 						}
@@ -84,7 +181,8 @@ public class CompilerComponentFactory {
 							posInLine++;
 						}
 						default -> {
-							tokens.add(new Token(Kind.NUMLIT, startPos, pos - startPos, line, startPosInLine));
+							result.add(new Token(Kind.INT_LITERAL, startPos, pos - startPos, line, startPosInLine, txt));
+							result.current = result.current.next;
 							state = State.START; 
 							//DO NOT INCREMENT pos  
 							
@@ -99,13 +197,15 @@ public class CompilerComponentFactory {
 		}
 		
 		//add EOF token
-		tokens.add(new Token(Kind.EOF, pos, 0, line, posInLine));
+		result.add(new Token(Kind.EOF, pos, 0, line, posInLine, ""));
+		result.current = result.head;
 		
-		
-		
-		return null;
+		return result;
+
 	}
-	
+
+
+
+
 
 }
-
